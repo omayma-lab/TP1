f6372f8fadb3ce16a9a32167680c37eb421316f0
omayma <ojaa09411@gmail.com>
2025-12-04 23:37:49 +0100

    first commit

---
+++ src/test/java/com/omayma/calculator/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.omayma.calculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class CalculatorTest {
+    @Test
+    public void testAdd(){
+        Calculator calculator= new Calculator();
+        int result= calculator.add(2,3);
+        assertEquals(5,result,"2+3 doit etre égal a 5");
+    }
+}
