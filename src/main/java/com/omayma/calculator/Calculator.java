437cfac4c6fc16243feab735a62d20333aafc491
omayma <ojaa09411@gmail.com>
2025-12-07 16:22:04 +0100

    par des méthodes static

--- src/main/java/com/omayma/calculator/Calculator.java
+++ src/main/java/com/omayma/calculator/Calculator.java
@@ -2,10 +2,10 @@ package com.omayma.calculator;
 
 
 public class Calculator {
-    public int add(int opG,int opD){
+    public static int add(int opG,int opD){
         return opG+opD;
     }
-public int divide(int opG,int opD){
+public static int divide(int opG,int opD){
        return opG/opD;
 }
 }
--- src/test/java/com/omayma/calculator/CalculatorTest.java
+++ src/test/java/com/omayma/calculator/CalculatorTest.java
@@ -1,35 +1,24 @@
 package com.omayma.calculator;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
-    private Calculator c;
-    @BeforeEach
-    void setUp(){
-        c= new Calculator();
-        System.out.println("---Début du test ---");
-    }
-    @AfterEach
-    void tearDown(){
-        System.out.println("--- Fin du test ---");
-    }
+
         @Test
         public void testAddition(){
 
-            assertEquals(7, c.add(3,4));
+            assertEquals(7, Calculator.add(3,4));
         }
         @Test
         public void testDivision(){
 
-            assertEquals(5, c .divide(10,2));
+            assertEquals(5, Calculator.divide(10,2));
         }
         @Test
 
         public void testDivisionByZero() {
 
-            assertThrows(ArithmeticException.class, () -> c.divide(10, 0));
+            assertThrows(ArithmeticException.class, () -> Calculator.divide(10, 0));
         }
 
     }
