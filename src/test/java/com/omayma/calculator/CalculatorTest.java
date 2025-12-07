05198a4d5f0834a317270d4c4756b2f28e8ac1f3
omayma <ojaa09411@gmail.com>
2025-12-07 16:13:14 +0100

    Ajouter des méthodes de montage/démontage

--- src/test/java/com/omayma/calculator/CalculatorTest.java
+++ src/test/java/com/omayma/calculator/CalculatorTest.java
@@ -1,22 +1,34 @@
 package com.omayma.calculator;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
+    private Calculator c;
+    @BeforeEach
+    void setUp(){
+        c= new Calculator();
+        System.out.println("---Début du test ---");
+    }
+    @AfterEach
+    void tearDown(){
+        System.out.println("--- Fin du test ---");
+    }
         @Test
         public void testAddition(){
-            Calculator c = new Calculator();
+
             assertEquals(7, c.add(3,4));
         }
         @Test
         public void testDivision(){
-            Calculator c =new Calculator();
+
             assertEquals(5, c .divide(10,2));
         }
         @Test
+
         public void testDivisionByZero() {
-            Calculator c = new Calculator();
+
             assertThrows(ArithmeticException.class, () -> c.divide(10, 0));
         }
 
